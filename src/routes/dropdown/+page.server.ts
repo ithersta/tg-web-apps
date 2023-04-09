2bafeb50f6dce3efd6d3045c21c2b1fb8bf1708c
Владислав Шиляев <schilyaev.vlad@gmail.com>
2023-04-09 13:04:01 +0300

    Retry request on failure

--- src/routes/dropdown/+page.server.ts
+++ src/routes/dropdown/+page.server.ts
@@ -1,6 +1,13 @@
+import pRetry, {AbortError} from "p-retry";
+
 /** @type {import('./$types').PageServerLoad} */
 export async function load({url}) {
     const source = url.searchParams.get('options');
-    const options = (await fetch(source)).json()
+    const options = await pRetry<any>(async () => {
+        const response = await fetch(source)
+        return await response.json().catch(() => {
+            throw new AbortError(response.statusText)
+        })
+    }, {retries: 5})
     return {options}
 }
