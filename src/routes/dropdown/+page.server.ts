4aba51578a70b5a18af37d69df77bdb52f2ae5a1
Владислав Шиляев <schilyaev.vlad@gmail.com>
2023-04-09 15:42:52 +0300

    Remove abort error

--- src/routes/dropdown/+page.server.ts
+++ src/routes/dropdown/+page.server.ts
@@ -1,13 +1,11 @@
-import pRetry, {AbortError} from "p-retry";
+import pRetry from "p-retry";
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load({url}) {
     const source = url.searchParams.get('options');
     const options = await pRetry<any>(async () => {
         const response = await fetch(source)
-        return await response.json().catch(() => {
-            throw new AbortError(response.statusText)
-        })
+        return await response.json()
     }, {retries: 5})
     return {options}
 }
